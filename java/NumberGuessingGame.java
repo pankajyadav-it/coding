21132cf7dc3b02d810ca5d9273ceecd8f6dfb597
Pankaj Yadav <ypankaj9116@gmail.com>
2026-02-24 11:34:32 +0530

    Update all pattern program

--- java/NumberGuessingGame.java
+++ java/NumberGuessingGame.java
@@ -1,7 +1,27 @@
 import java.util.*;
 public class NumberGuessingGame {
+    public static void main(String[] args) {
+        
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    
-    
+    Random rand = new Random();
+    int number = rand.nextInt(100) + 1;
+    int guess = 0;
+    System.out.println("guess a number between 1 and 100 :");
+    while(guess != number){
+        System.out.print("Enter your guess :");
+        guess = sc.nextInt();
+        if(guess < number){
+            System.out.println("Too low! Try again.");
+        } else if(guess > number){
+            System.out.println("Too high! Try again.");
+        } else {
+            System.out.println("Congratulations! You guessed the number!");
+        }
+    }
+
+
+
+
+ }
+  
 }
